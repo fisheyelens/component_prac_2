193eaa5286903006eaced68268b20faded9e8cfa
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-13 00:21:39 +0900

    tostring test & using typeof

---
+++ test-2.js
@@ -0,0 +1,13 @@
+// * 데이터 타입 변환 test 창입니다.
+
+var x = 10;
+
+var str = x.toString();
+console.log(typeof str,str);  // string , 10 나온다
+// * 아 헐 log()에 한가지 이상 주문 넣을 수 있구나?!! 쉽표 써서!
+console.log(str); // 10 나온다
+
+console.log(typeof x, x); // number, 10 나온다
+// * 결론 -> x 변수의 값이 변경된 것은 아니다. 
+// * x = ver.1  str = ver.2 정도로 생각하면 될 듯?
+
