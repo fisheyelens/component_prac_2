d5f720a5d77bbe59f477c1dbad66b6c0d005a348
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-12 17:17:40 +0900

    date 찍는 함수 test 한 것

--- dateTest.js
+++ dateTest.js
@@ -1,25 +1,55 @@
-function testing () {
+// function testing () {
 
   
-  today.setDate(1);
-  today.getDate();
+//   today.setDate(1);
+//   today.getDate();
+//   }
+  
+//   console.log(testing());
+  
+// // const please = {
+  
+//   const today = Date(day);
+
+//   console.log(today);
+
+
+// const try1 = Date.now(day);
+// const try1 = new Date(day[]);
+
+// console.log(try1);
+
+
+// * 왜이렇게 꼬아놨지; 
+// function lalala () {
+// const now =  Date.now();
+
+// // console.log(now); // * 이상한 숫자들 나옴
+// new Date(now);
+// return new Date; // *  리턴 까먹었었네 ㅠ
+
+// }
+// console.log(lalala()); // * 2024-06-12 + 시간 나옴
+
+
+function lalala () {
+
+  return new Date; 
+  
   }
   
-  console.log(testing());
-  
-// const please = {
-  
-  const today = Date(day);
+  // console.log(lalala()); // * 2024-06-12 + 시간 나옴
+let test = lalala();
+console.log(test);  // * 2024-06-12 + 시간 나옴
 
-  console.log(today);
+let innerFunc = () => {
+  // if(new Date(test) === 12 ) {
+  // if(test === new Date("2024-06-12")) {
+  if(test === "2024-06-12") {
+    return "오우 12일이구나"    
+  } else { "잉 오늘 12일 아닌뎀"
 
-
-
-
-  function plus (param) {
-    return param === ()
   }
+}
 
-  let test = plus();
-
-  console.log(test);
+console.log(innerFunc());
