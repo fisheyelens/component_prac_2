8c9326132f51f42549dde7b9cef9a747658ab143
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-12 15:45:31 +0900

    shitty trial

---
+++ dateTest.js
@@ -0,0 +1,25 @@
+function testing () {
+
+  
+  today.setDate(1);
+  today.getDate();
+  }
+  
+  console.log(testing());
+  
+// const please = {
+  
+  const today = Date(day);
+
+  console.log(today);
+
+
+
+
+  function plus (param) {
+    return param === ()
+  }
+
+  let test = plus();
+
+  console.log(test);
