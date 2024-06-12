c33bc22a22b7d86c93fc70bb1e72ff02d26a7104
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-12 10:44:46 +0900

    component experience 1

--- work-1.js
+++ work-1.js
@@ -1,10 +1,58 @@
+// 기본
+// const basicData = {
+//   name : "",
+//   age: 0,
+//   job : ""
+// }
 
-const basicData = {
-  name : "",
-  age: 0,
-  job : ""
+// const component = () => {
+//  let obj = {}; 
+
+
+// const decision = () => {};
+
+// return `<html></html>`;
+// }
+
+
+
+// 추가 시작
+// const basicData = {
+//   name : "",
+//   age: 0,
+//   job : ""
+// }
+
+
+// const component = () => {
+//   const basicData = {
+//     name : "",
+//     age: 0,
+//     job : "" 이렇게 넣을 수도 있는데 이것은 정적이라 다시 빼기
+  
+//   }
+
+
+const basicData = () => {
+
+return {
+  name : "백지원",
+  age: 100,
+  job : "학생"
+  }
 }
+  
+
 
 const component = () => {
-  
-}
+  let obj = basicData();
+  }
+
+
+// return 이 객체니까 let obj 의 obj 가 객체겠거니
+// * 정적인 함수를 깨기 위해서 바깥에 다른 함수 호출출
+
+
+const decision = () => {};
+
+return `<html></html>`;
